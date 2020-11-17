e21633eacb68b34281c0404ba6b5c60ec932b945
Tanzim Absar <tanzim_101@live.com>
2020-11-17 21:28:33 +0000

    added count negative numbers

--- Search/binary_search.py
+++ Search/binary_search.py
@@ -3,16 +3,32 @@ from typing import List
 """ Assume that the array is already sorted and then halve the list """
 
 
-def binart_search(iterable: List, index: int):
+def binary_search(array, element, start, end):
 
-    middle = len(iterable) // 2
+    if start > end:
+        # In this case we know that the array is not valid
+        return None
+    
+    # find the middle
+    mid = (start + end) // 2
 
-    if iterable[middle] == index:
-        return middle
-    elif iterable[middle] > index:
-        return "item is in the other half"
+    # if the element you are trying to find is in that middle index return the index
+    # this is the case that it is trying to find 
+    if element == array[mid]:
+        return mid
+
+    # if it is list then at that index, recursively call binary search 
+    # but the last index is one less than the mid point
+
+    if element < array[mid]:
+        return binary_search(array, element, start, mid-1)
+
+        # otherwise start on the second half because you know it lies on that side
+    
+    else:
+        return binary_search(array, element, mid+1, end)
+
+print(binary_search([1,2,3,4,5], 5, 1, 5))
 
 
-def test_linear_search():
 
-    print(binart_search([0, 3, 4, 44, 55, 66, 77, 88, 99, 112], 4))
